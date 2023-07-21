4a5a3971d792583db3ef03c28c73e328353c39a5
Mahesh Bhadane <108877616+MaheshBhadane@users.noreply.github.com>
2023-07-22 02:17:19 +0530

    Footer image and details added

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,17 @@
 import { useAppDispatch, useAppSelector } from "@/app/hooks";
 import { prevSong, setPlaying, nextSong } from "@/reducers/songsSlice";
-import { Button, Group } from "@mantine/core";
+import { Button, Group, Image } from "@mantine/core";
 import {
   IconPlayerPause,
   IconPlayerPlayFilled,
   IconPlayerTrackPrevFilled,
   IconPlayerTrackNextFilled,
 } from "@tabler/icons-react";
-import { StyledFooter } from "@/components/Footer/style";
+import { StyledFooter, StyledGroup } from "@/components/Footer/style";
 import { useEffect, useRef, useState } from "react";
 import { formatTime } from "@/utils";
 import { Volume2, VolumeX, Volume1 } from "lucide-react";
-
+import userPic from "@/assets/musician.png";
 
 const FooterPage = () => {
   const dispatch = useAppDispatch();
@@ -51,47 +51,85 @@ const FooterPage = () => {
   }, [currentSong]);
 
   return (
-    <StyledFooter height={70} withBorder={false}>
-      <Group>
-        <span>{formatTime(currentTime)}</span>
-        <input
-          type="range"
-          value={currentTime}
-          max={duration}
-          step="0.01"
-          onChange={(e) => {
-            audioRef.current.currentTime = parseFloat(e.target.value);
-            setCurrentTime(parseFloat(e.target.value));
-          }} />
-        <span>{formatTime(duration)}</span>
-        <audio
-          id="audio1"
-          src={currentSong?.previewUrl}
-          ref={audioRef}
-          autoPlay
-          onTimeUpdate={() => setCurrentTime(audioRef?.current?.currentTime)}
-          onDurationChange={() => setDuration(audioRef.current.duration)}
-          onEnded={() => {
-            setCurrentTime(audioRef?.current?.duration);
-            dispatch(setPlaying(false));
+    <StyledFooter height={80} withBorder={false}>
+      <Group style={{ alignItems: "center", gap: "1rem" }}>
+        <Image
+          src={currentSong ? currentSong?.artworkUrl100 : userPic}
+          alt="song"
+          style={{
+            height: "3rem",
+            width: "3rem",
           }}
         />
+        <div>
+          <h6>{currentSong?.trackName}</h6>
+          <h6>{currentSong?.artistName}</h6>
+        </div>
       </Group>
-      <Group style={{ flex: 1, justifyContent: "center" }}>
-        <Button variant="subtle" color="dark" size="lg" onClick={handlePrevSongClick}>
-          <IconPlayerTrackPrevFilled size={"30px"} />
-        </Button>
-        <Button variant="subtle" color="dark" size="lg" onClick={handleFooterButtonClick}>
-          {isPlaying ? <IconPlayerPause size={"40px"} /> : <IconPlayerPlayFilled size={"40px"} />}
-        </Button>
-        <Button variant="subtle" color="dark" size="lg" onClick={handleNextSongClick}>
-          <IconPlayerTrackNextFilled size={"30px"} />
-        </Button>
-      </Group>
-      <Group>          
+
+      <audio
+        id="audio1"
+        src={currentSong?.previewUrl}
+        ref={audioRef}
+        autoPlay
+        onTimeUpdate={() => setCurrentTime(audioRef?.current?.currentTime)}
+        onDurationChange={() => setDuration(audioRef.current.duration)}
+        onEnded={() => {
+          setCurrentTime(audioRef?.current?.duration);
+          dispatch(setPlaying(false));
+        }}
+      />
+      <StyledGroup>
+        <Group>
+          <Button
+            variant="subtle"
+            color="dark"
+            size="lg"
+            onClick={handlePrevSongClick}
+          >
+            <IconPlayerTrackPrevFilled size={"30px"} />
+          </Button>
+          <Button
+            variant="subtle"
+            color="dark"
+            size="lg"
+            onClick={handleFooterButtonClick}
+          >
+            {isPlaying ? (
+              <IconPlayerPause size={"40px"} />
+            ) : (
+              <IconPlayerPlayFilled size={"40px"} />
+            )}
+          </Button>
+          <Button
+            variant="subtle"
+            color="dark"
+            size="lg"
+            onClick={handleNextSongClick}
+          >
+            <IconPlayerTrackNextFilled size={"30px"} />
+          </Button>
+        </Group>
+        <Group style={{ alignItems: "center" }}>
+          <span>{formatTime(currentTime)}</span>
+          <input
+            type="range"
+            value={currentTime}
+            max={duration}
+            step="0.01"
+            onChange={(e) => {
+              audioRef.current.currentTime = parseFloat(e.target.value);
+              setCurrentTime(parseFloat(e.target.value));
+            }}
+          />
+          <span>{formatTime(duration)}</span>
+        </Group>
+      </StyledGroup>
+
+      <Group style={{ alignItems: "center", gap: "1rem" }}>
         {volume <= 1 && volume > 0.5 ? (
-        <Volume2 color="black" size={25} onClick={() => setVolume(0.5)} />
-      ) : null}
+          <Volume2 color="black" size={25} onClick={() => setVolume(0.5)} />
+        ) : null}
         {volume <= 0.5 && volume > 0 ? (
           <Volume1 color="black" size={25} onClick={() => setVolume(1)} />
         ) : null}
