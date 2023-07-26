8b46ff7e50ec1bbd16d6ff874bf567e252954ccb
Mahesh Bhadane <108877616+MaheshBhadane@users.noreply.github.com>
2023-07-26 20:59:11 +0530

    INTG: Optimised Custom Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,31 +3,47 @@ import {
     ButtonProps as MantineButtonProps,
 } from "@mantine/core";
 import { ComponentPropsWithoutRef } from "react";
+import Loader from "./Loader";
 
 interface ButtonProps
     extends MantineButtonProps,
     Omit<ComponentPropsWithoutRef<"button">, "color"> {
-
+    isLoading?: boolean,
+    btnType?: string
 }
 
 const Button = (props: ButtonProps) => {
     const {
         children,
+        fullWidth,
+        className,
+        btnType = 'primary',
         variant = "outline",
         color = "gray",
         size = "md",
         radius = "md",
+        type = 'button',
+        isLoading = false,
+        disabled,
+        onClick,
+        value
     } = props;
 
     return (
         <MantineButton
             {...props}
+            className={`${btnType} size-${size} ${className ?? ''} ${fullWidth ? 'fullWidth' : ''}`}
             variant={variant}
             color={color}
             size={size}
             radius={radius}
+            type={type}
+            disabled={disabled}
+            loading={isLoading}
+            onClick={onClick}
         >
             {children}
+            {isLoading ? <Loader /> : value}
         </MantineButton>
     );
 };
